fd98d24c6725d2c02181957294359bd23d1682d1
Damien Baty <damien@damienbaty.com>
2020-03-30 16:17:17 +0200

    demo: Rewrite and greatly simplify, properly fetch requirements
    1. A single view and a single template for all forms. This is simpler.
    
    2. Properly fetch CSS and JavaScript requirements from the form.

--- demo/app.py
+++ demo/app.py
@@ -10,6 +10,7 @@ Instructions:
 An HTTP server will listen on port 61523: http://0.0.0.0:61523
 """
 
+import json
 import os
 
 from colander import Schema
@@ -19,6 +20,7 @@ from colander import String
 
 from deform import Form
 from deform import ValidationFailure
+from deform.widget import HiddenWidget
 from deform.widget import SequenceWidget
 
 from pyramid.config import Configurator
@@ -34,78 +36,99 @@ PERSONS = {'jhendrix': 'Jimi Hendrix',
            'bcobham': 'Billy Cobham'}
 
 
+PersonNode = SchemaNode(
+    String(),
+    widget=ExtendedAutocompleteInputWidget(
+        display_value=lambda field, person_id: PERSONS.get(person_id, ''),
+        values='/ajax_search')
+)
+
+
 class Person(SequenceSchema):
-    person = SchemaNode(
-        String(),
-        widget=ExtendedAutocompleteInputWidget(
-            display_value=lambda field, person_id: PERSONS.get(person_id, ''),
-            values='/ajax_search'))
+    person = PersonNode
 
 
-class DemoSchema(Schema):
+class DemoSchemaMultiple(Schema):
     persons = Person(widget=SequenceWidget(min_len=1))
 
 
-def make_form(request, action):
-    schema = DemoSchema().bind(request=request)
-    return Form(schema, action=action, buttons=('Save changes', ))
+class DemoSchemaSingle(Schema):
+    person = PersonNode
 
 
-def add_form(request, form=None):
-    if form is None:
-        form = make_form(request, request.route_url('add'))
+def make_form(request, form_class, form_name):
+    schema = form_class().bind(request=request)
+    schema.add(
+        SchemaNode(
+            String(),
+            name='submitted_form',
+            default=form_name,
+            widget=HiddenWidget(),
+        )
+    )
+    action = request.route_url('index')
+    return Form(schema, action=action, buttons=['Save'])
+
+
+def index(request):
+    # Initialize all forms.
+    add_form_single = make_form(request, DemoSchemaSingle, 'add_single')
+    add_form_multiple = make_form(request, DemoSchemaMultiple, 'add_multiple')
+    edit_form_multiple = make_form(request, DemoSchemaMultiple, 'edit_multiple')
+    read_only_form = make_form(request, DemoSchemaMultiple, 'read_only')
+
+    # Process submitted form (if any).
+    submitted_form = None
+    if request.method == 'POST':
+        submitted_form_name = request.POST['submitted_form']
+        submitted_form = {
+            'add_single': add_form_single,
+            'add_multiple': add_form_multiple,
+            'edit_multiple': edit_form_multiple,
+        }.get(submitted_form_name)
+        if submitted_form is None:
+            raise ValueError("Unexpected submitted form: '%s'" % submitted_form)
+
+        try:
+            data = submitted_form.validate(request.POST.items())
+        except ValidationFailure:
+            pass
+        else:
+            data.pop('submitted_form')
+            url = request.route_url('saved', _query='saved=%s' % json.dumps(data))
+            return HTTPSeeOther(url)
+
+    # Get requirements before rendering the forms. All forms have the
+    # same requirements.
+    requirements = add_form_multiple.get_widget_resources()
+
+    # Pre-fill read-only and edit forms (the latter only if it has not
+    # been submitted). Render all forms for the template.
+    form_data = {'persons': ('jpage', 'jbonham')}
+    read_only_form = read_only_form.render(form_data, readonly=True)
+    if submitted_form != edit_form_multiple:
+        edit_form_multiple = edit_form_multiple.render(form_data)
+    else:
+        edit_form_multiple = edit_form_multiple.render()
+    add_form_single = add_form_single.render()
+    add_form_multiple = add_form_multiple.render()
+
     return {
         'request': request,
-        'requirements': form.get_widget_resources(),
-        'form': form.render(),
+        'requirements': requirements,
         'persons': sorted(PERSONS.values()),
+        'forms': {
+            'add_single': add_form_single,
+            'add_multiple': add_form_multiple,
+            'edit_multiple': edit_form_multiple,
+            'read_only': read_only_form,  # already rendered
+        },
     }
-
-
-def _validate_and_redirect(request, view, route):
-    """Validate form and redirect to the add/edit form or the success
-    page.
-    """
-    form = make_form(request, request.route_url(route))
-    try:
-        data = form.validate(request.POST.items())
-    except ValidationFailure as e:
-        return view(request, e)
-    url = request.route_url('saved', _query={'saved': data})
-    return HTTPSeeOther(url)
-
-
-def save(request):
-    return _validate_and_redirect(request, add_form, 'add')
 
 
 def saved(request):
-    return {'request': request,
-            'saved': request.GET['saved']}
-
-
-def edit_form(request, form=None):
-    if form is None:
-        form = make_form(request , request.route_url('edit'))
-        form_data = {'persons': ('jpage', 'jbonham')}
-        form = form.render(form_data)
-    else:
-        form = form.render()
-    return {
-        'requirements': form.get_widget_resources(),
-        'form': form
-    }
-
-
-def edit(request):
-    return _validate_and_redirect(request, edit_form, 'edit')
-
-
-def readonly(request):
-    form = make_form(request , 'there-is-no-action')
-    form_data = {'persons': ('jpage', 'jbonham')}
-    form = form.render(form_data, readonly=True)
-    return {'rendered_form': form}
+    saved = json.loads(request.GET['saved'])
+    return {'saved': saved}
 
 
 def highlight_term(term, s, pattern='<strong>%s</strong>'):
@@ -146,31 +169,27 @@ def get_template_path(filename):
 
 
 def make_app(global_config, **settings):
-    add_pt = get_template_path('add.pt')
-    edit_pt = get_template_path('edit.pt')
+
     saved_pt = get_template_path('saved.pt')
-    readonly_pt = get_template_path('readonly.pt')
+
     config = Configurator(settings=settings)
-    config.add_route('add', '/')
-    config.add_view(add_form, route_name='add',
-                    request_method='GET',
-                    renderer=add_pt)
-    config.add_view(save, route_name='add',
-                    request_method='POST',
-                    renderer=add_pt)
+
+    config.add_route('index', '/')
+    config.add_view(
+        index,
+        route_name='index',
+        request_method=('GET', 'POST'),
+        renderer=get_template_path('index.pt'),
+    )
+
     config.add_route('saved', '/saved')
     config.add_view(saved, route_name='saved',
                     renderer=saved_pt)
-    config.add_route('edit', '/edit')
-    config.add_view(edit_form, route_name='edit', request_method='GET',
-                    renderer=edit_pt)
-    config.add_view(edit, route_name='edit', request_method='POST',
-                    renderer=edit_pt)
-    config.add_route('readonly', '/readonly')
-    config.add_view(readonly, route_name='readonly', renderer=readonly_pt)
+
     config.add_route('ajax_search', '/ajax_search')
     config.add_view(ajax_search, route_name='ajax_search',
                     xhr=True, renderer='json')
+
     config.add_static_view('static', 'deform:static')
     includeme(config)
     return config.make_wsgi_app()
